fd16341587e543b07c2282b039754efd2d9bf800
Joel Guerrero <joelgur10@hotmail.com>
2024-11-07 20:46:19 -0500

    Readme is added, it is change in structure

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
-from app.routes.category_routes import category_router
-from app.routes.location_routes import location_router
-from app.routes.recommendations_router import recommender_router
+from app.routes.exploration_recommender_router import recommender_router
+from app.routes.review_locations_router import review_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Exploration Recommender API",
+    description="API for exploration recommendations and location reviews.",
+    version="2.0.0",
+)
 
-app.include_router(category_router, prefix="/category", tags=["Category"])
-app.include_router(location_router, prefix="/location", tags=["Location"])
-app.include_router(recommender_router, prefix="/recommender", tags=["Recommender"])
+app.include_router(recommender_router, prefix="/exploration-recommendations", tags=["Exploration Recommender"])
+app.include_router(review_router, prefix="/review", tags=["Review"])
